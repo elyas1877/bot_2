141e19c674e123892ecef1bc7da6df85f74ee5c8
elyas1877 <elyas1877@gmail.com>
2021-08-11 02:03:59 +0430

    sql fixed

--- bot12/DB/conf.py
+++ bot12/DB/conf.py
@@ -2,11 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 # from bot import DATABASE_URL, LOGGER
+import os
 
 
 def start() -> scoped_session:
   try:
-    engine = create_engine('postgresql://wfjhajuqtgqoas:c42d169cacfc005ef1d88eae9c674423d57ba535c2aafeecf77ef7f7ad93b699@ec2-3-226-134-153.compute-1.amazonaws.com:5432/d9leiaac4kshrh')
+    uri = os.getenv("DATABASE_URL")  # or other relevant config var
+    if uri.startswith("postgres://"):
+      uri = uri.replace("postgres://", "postgresql://", 1)
+      
+    engine = create_engine(uri)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
