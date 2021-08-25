721152ffc7a4ff7afa938babac9b1572734f07df
elyas1877 <elyas1877@gmail.com>
2021-08-26 00:32:50 +0430

    edit

--- bot12/DB/conf.py
+++ bot12/DB/conf.py
@@ -12,6 +12,7 @@ def start() -> scoped_session:
       
     if uri.startswith("postgres://"):
       uri = uri.replace("postgres://", "postgresql://", 1)
+    # uri = 'postgresql://wfjhajuqtgqoas:c42d169cacfc005ef1d88eae9c674423d57ba535c2aafeecf77ef7f7ad93b699@ec2-3-226-134-153.compute-1.amazonaws.com:5432/d9leiaac4kshrh'
     engine = create_engine(uri)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
