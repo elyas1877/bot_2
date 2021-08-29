298168a2103926e73badf356bac8b941c9d8a6ef
elyas1877 <elyas1877@gmail.com>
2021-08-29 15:40:08 +0430

    mistake

--- bot12/Upload.py
+++ bot12/Upload.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import pickle
 from googleapiclient.http import MediaFileUpload
-from pytube import YouTube
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
